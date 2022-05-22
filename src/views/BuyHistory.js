659497fdd0b30a60f6b1b4fbe27a875df4f2d62b
Chamuditha Weerasinghe <80471215+Chamudithawee@users.noreply.github.com>
2022-05-22 11:07:57 +0530

    Update BuyHistory.js

--- src/views/BuyHistory.js
+++ src/views/BuyHistory.js
@@ -1,66 +1,77 @@
-import{Button} from 'reactstrap'
-import{ useHistory } from "react-router-dom"
+// reactstrap components
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from 'react-toastify';
+import { useHistory } from "react-router-dom";
+import styles from "../assets/css/ProfilePage.module.css"
 import { jsPDF } from "jspdf";
-import { useEffect } from "react/cjs/react.development";
-import { useState } from 'react';
 
+import {
+  Input,
+  Row,
+  Col,
+  Button
+} from "reactstrap";
 
+// core components
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
 import ProfilePageHeader from "components/Headers/ProfilePageHeader.js";
 import DefaultFooter from "components/Footers/DefaultFooter.js";
-import { ReactSession } from "react-client-session";
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
-import {
-    Label,
-    Input,
-    Row,
-    Col,
-    InputGroup,
-    InputGroupAddon,
-    InputGroupText,
-    FormGroup,
-    Card,
-    Alert,
-    Container,
-  } from "reactstrap";
 
-toast.configure();
+function BuyHistory () {
 
-function BuyHistory({}) {
-
-    let history = useHistory();
-    const [user, setuser] = useState();
-    let doc;
-
-    const [BuyHistory , setBuyHistory] = useState([]);
+    const [products, setProducts] = useState([]);
     const [searchVal , setSearchVal] = useState("");
 
     useEffect(() => {
-
-
-        ReactSession.setStoreType("localstorage");
-        if (ReactSession.get("user") === null) {
-          history.push({
-            pathname: "/login",
+      function getHistory() {
+          axios.get("http://localhost:8070/user-report/").then((res) => {
+            setProducts(res.data);
+            console.log(res);
+          }).catch((err) => {
+            alert("Something went wrong :(");
+            alert(err.message);
           });
-        }
-  
-        else {
+      };
+        getHistory();
+    },[]);
 
 
-        axios.get('http://localhost:8070/users/report').then((res) =>{
-          setBuyHistory(res.data);
-            console.log(res.data);
-        }).catch((err) =>{
-            console.log(err);
-        })
-    };
-    
-      }, []);
+    const historyDelete = (del) => {
 
-      var number = 1;
+      if (
+        window.confirm(
+          "Confirm to remove?"
+        )
+      )
+      axios.delete(`http://localhost:8070/user-report/delete-history/${del._id}`)
+      .then((res) =>{
+          console.log(res);
+          toast.success("Record Deleted!", {
+              position: "bottom-right",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              });     
+      }).catch((err) =>{
+          console.log(err);
+          alert(err);
+          toast.error("Something went wrong :(", {
+            position: toast.POSITION.BOTTOM_RIGHT,
+            autoClose: 10000,
+            hideProgressBar: false,
+          });
+      });
+    }
+
+
+      let history = useHistory();
+      let doc;
+
 
       const downloadPDF = () => {
  
@@ -75,130 +86,105 @@ function BuyHistory({}) {
           },
         });
       };
-      
- return (
-     <>
-     <ExamplesNavbar />
-      <div className="wrapper">
-        <ProfilePageHeader />
-        < br/>
-        < br/>
-            <h3 ><center>Buy Report</center></h3><br/><br/>
-   
 
-    <Container>
-<br/><br/>
-<div style = {{marginLeft : "40px" , marginRight : "40px" }}>
-<div style = {{display : "flex" , flexDirection : "row" }}>
-<div style = {{width : "30%" }}>
+
+      var number = 1;
+
+      return (
+        <>
+        <ExamplesNavbar />
+        <div className="wrapper">
+
+        <ProfilePageHeader />
+
+<div id="report-cont" className = "container">
+    <h3 style = {{marginLeft:"430px"}}><b>Buy History</b></h3>
+    <Input placeholder="Search " type="text" 
+    onChange = {(e) =>{
+      setSearchVal(e.target.value);
+    }}/>
+    {
+        products.filter((BuyHistory) => {
+            let OrderName = BuyHistory.OrderName;
+              if (searchVal === "") {
+                return BuyHistory;
+              } else {
+                if (OrderName) {
+                  if (
+                    OrderName.toLowerCase().includes(searchVal.toLowerCase())
+                  ) {
+                    return BuyHistory;
+                  }
+                }
+              }
+        })
+        .map((BuyHistory) => (
+            <div style = {{marginLeft:"20px"}}  className = "tableContainer">
+            <table className = "table table-striped">
+                <thead>
+                <th scope = "col"> No </th>
+                    <th scope = "col"> OrderID </th>
+                    <th scope = "col"> Customer Email </th>
+                    <th scope = "col"> Order Name </th>
+                    <th scope = "col"> Quantity </th>
+                    <th scope = "col"> Price </th>
+                    <th scope = "col"> Date </th>
+          
+                </thead>
+                <tbody>
+                
+                    <tr>
+                    <th scope = "row">{number++}</th>
+                    
+                    <td>{BuyHistory.OrderID}</td>
+
+                    <td>{BuyHistory.CustomerEmail}</td>
+                    
+                    <td>{BuyHistory.OrderName}</td>
+                    
+                    <td>{BuyHistory.Quantity}</td>
+                    
+                    <td>{BuyHistory.Price}</td>
+
+                    <td>{BuyHistory.Date}</td>
+
+                    <td>
+
+                        <Button color="danger" style = {{padding: "5px 5px 5px 5px", width : "70px", marginBottom : "8px"}}
+                        onClick = {() =>
+
+                          historyDelete(BuyHistory)
+                        }
+                        
+                        >Remove</Button>
+                        </td>
+                        </tr>
+                
+                </tbody>
+            </table>
+            </div> 
+        ))
+    }
+</div>
+
+<div className="report-download">
+              <Row>
+                <Col>
+                  <button
+                    className="btn btn-info"
+                    onClick={downloadPDF}
+                  >
+                    Download PDF
+                  </button>
+                </Col>
+              </Row>
+          </div>
 
         </div>
-
-</div>
-<Col>
-    <FormGroup>
-      <InputGroup style = {{marginLeft : "70px"}} className="form-group-no-border">
-        <InputGroupAddon addonType="prepend">
-          <InputGroupText>
-            <i className="nc-icon nc-zoom-split" />
-          </InputGroupText>
-        </InputGroupAddon>
-        <Input placeholder="Search " type="text" 
-            onChange = {(e) =>{
-                        setSearchVal(e.target.value);
-            }}/>
-      </InputGroup>
-    </FormGroup>
-  </Col>
-<hr></hr>
-
-<div id ="report-cont" >
-        <Card className="" id="report" style = {{padding : "20px"}}>
-            <Row>
-              <Col>
-                {"../assets/img/shopsy.png "}
-                <img
-                  style = {{width : "30%" , marginLeft : "35%" }}
-                  src={
-                    require().default
-                  }
-                  className="report-logo"
-                />
-              </Col>
-            </Row>
-           
-            <br/><br/>
-
-            <div style = {{marginLeft:"20px"}}  className = "tableContainer">
-        <table className = "table">
-            <thead>
-            <th scope = "col">#</th>
-                <th scope = "col">Order ID</th>
-                <th scope = "col">Order Name</th>
-                <th scope = "col">Quantity</th>
-                <th scope = "col">Date</th>
-                <th scope = "col">Price</th>
-            </thead>
-
-            <tbody>
-                
-                {BuyHistory.filter((val) =>{
-                    if(searchVal === ''){
-                      return val;
-                    }
-                    else if (val.fullName.toLowerCase().includes(searchVal.toLowerCase())){
-                      return val;
-                    }
-                    else if (val.insurance.toLowerCase().includes(searchVal.toLowerCase())){
-                      return val;
-                    }
-    
-
-                }).map((BuyHistory)=>(
-                        <tr>
-                       <th scope = "row">{number++}</th>
-                        <td>{BuyHistory.OrderID}</td>
-                        <td>{BuyHistory.OrderName}</td>
-                        <td>{BuyHistory.Quantity}</td>
-                        <td>{BuyHistory.Date}</td>
-                        <td>{BuyHistory.Price}</td>
-                        </tr>
-
-                  ))}
-            </tbody>    
-
-
-        </table>
-    </div>  
-  </Card> 
-  </div>
-  <div className="report-download">
-      <Row>
-        <Col>
-          <button
-            className="btn btn-info"
-            onClick={downloadPDF}
-          >
-            Download PDF
-          </button>
-        </Col>
-      </Row>
-  </div>
-</div>
-
-</Container>
-
-
-    </div>
-    
-    <DefaultFooter />    
-
-
-
-</>
-    
-);
-
-
+        <DefaultFooter />
+        
+        </>
+      )
 }
+
 export default BuyHistory;
